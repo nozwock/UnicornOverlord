nozwock/UnicornOverlord

commit 5f9dcd118c45780be8d42bacea41053942005815

Add a search bar to "Items" tab

UnicornOverlord/Item.cs
@@ -28,6 +28,16 @@ namespace UnicornOverlord
 			}
 		}
 
+		private string? _name;
+		public string Name
+		{
+			get
+			{
+				_name ??= Info.Instance().Search(Info.Instance().Item, ID)?.Name ?? ID.ToString();
+				return _name;
+			}
+		}
+
 		public uint Index
 		{
 			get => SaveData.Instance().ReadNumber(mAddress + 4, 4);

UnicornOverlord/MainWindow.xaml
@@ -180,9 +180,25 @@
 			</TabItem>
 			<TabItem Header="Item">
 				<DockPanel>
-					<Button DockPanel.Dock="Bottom" Content="Append" Command="{Binding AppendItemCommand}"/>
-					<Button DockPanel.Dock="Top" Content="Count Max" Command="{Binding ChangeItemCountMaxCommand}"/>
-					<ListBox ItemsSource="{Binding Items}">
+					<Grid DockPanel.Dock="Top">
+						<Grid.ColumnDefinitions>
+							<ColumnDefinition Width="Auto"/>
+							<ColumnDefinition Width="*"/>
+						</Grid.ColumnDefinitions>
+						<Label
+							Grid.Column="0"
+							Content="Filter"
+							VerticalAlignment="Center"/>
+						<TextBox
+							Grid.Column="1"
+							Text="{Binding ItemFilterText, UpdateSourceTrigger=PropertyChanged}"
+							VerticalContentAlignment="Center"/>
+					</Grid>
+					<UniformGrid DockPanel.Dock="Bottom" Rows="1" Columns="2">
+						<Button Content="Append" Command="{Binding AppendItemCommand}"/>
+						<Button Content="Count Max" Command="{Binding ChangeItemCountMaxCommand}"/>
+					</UniformGrid>
+					<ListBox ItemsSource="{Binding ItemsView}">
 						<ListBox.ItemTemplate>
 							<DataTemplate>
 								<StackPanel Orientation="Horizontal">
@@ -191,7 +207,7 @@
 									<Button Content=" ... "
 									Command="{Binding RelativeSource={RelativeSource Mode=FindAncestor, AncestorType=local:MainWindow}, Path=DataContext.ChoiceItemCommand}"
 									CommandParameter="{Binding}"/>
-									<Label Content="{Binding ID, Converter={StaticResource ItemIDConverter}}"/>
+									<Label Content="{Binding Name}"/>
 								</StackPanel>
 							</DataTemplate>
 						</ListBox.ItemTemplate>

UnicornOverlord/ViewModel.cs
@@ -9,6 +9,7 @@ using System.Reflection;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace UnicornOverlord
@@ -39,6 +40,19 @@ namespace UnicornOverlord
 		public ObservableCollection<Item> Equipments { get; set; } = new ObservableCollection<Item>();
 		public ObservableCollection<Unit> Units { get; set; } = new ObservableCollection<Unit>();
 
+		public ICollectionView ItemsView { get; }
+		private string _itemFilterText = "";
+		public string ItemFilterText
+		{
+			get => _itemFilterText;
+			set
+			{
+				_itemFilterText = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ItemFilterText)));
+				ItemsView.Refresh();
+			}
+		}
+
 		public ViewModel()
 		{
 			OpenFileCommand = new ActionCommand(OpenFile);
@@ -55,6 +69,19 @@ namespace UnicornOverlord
 			ChangeItemCountMaxCommand = new ActionCommand(ChangeItemCountMax);
 			ChangeCharacterBondMaxCommand = new ActionCommand(ChangeCharacterBondMax);
 			ChangeCharacterBondMaxAllCommand = new ActionCommand(ChangeCharacterBondMaxAll);
+
+			ItemsView = CollectionViewSource.GetDefaultView(Items);
+			ItemsView.Filter = obj =>
+            {
+				if (obj is not Item item)
+					return false;
+				if (string.IsNullOrWhiteSpace(ItemFilterText))
+					return true;
+
+				return item.Name.Contains(
+					ItemFilterText,
+					StringComparison.OrdinalIgnoreCase);
+			};
 		}
 
 		private void Initialize()

commit 801fe4e56d21a8387f88f080ecea526deef0ffbf

Fix item name not being updated when replaced with another
Regression introduced in 5f9dcd118c45780be8d42bacea41053942005815.

UnicornOverlord/Item.cs
@@ -24,7 +24,9 @@ namespace UnicornOverlord
 			set
 			{
 				SaveData.Instance().WriteNumber(mAddress, 4, value);
+				_name = null;
 				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ID)));
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
 			}
 		}
 