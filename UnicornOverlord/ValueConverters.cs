8f3cc8b9e9f02c984c89f287a3e18619ec4202fb
nozwock <57829219+nozwock@users.noreply.github.com>
2026-01-21 13:34:39 +0530

    Remove unneeded value converters

--- UnicornOverlord/ValueConverters.cs
+++ UnicornOverlord/ValueConverters.cs
@@ -24,58 +24,12 @@ namespace UnicornOverlord
 		}
 	}
 
-	internal class ItemIDConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			uint id = (uint)value;
-			var item = Info.Instance().Search(Info.Instance().Item, id);
-			if (item == null) return id.ToString();
-			return item.Name;
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			throw new NotImplementedException();
-		}
-	}
-
-	internal class NameIDConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			uint id = (uint)value;
-			var nm = Info.Instance().Search(Info.Instance().Name, id);
-			if (nm == null) return id.ToString();
-			return nm.Name;
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			throw new NotImplementedException();
-		}
-	}
-
 	internal class ItemEquipSlotConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
 			var slot = (byte)value;
 			return slot == byte.MaxValue ? "N/A" : (slot + 1).ToString();
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			throw new NotImplementedException();
-		}
-	}
-
-	internal class ItemUnitEquippedIconIdConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			var id = (uint)value;
-			return id == uint.MaxValue ? "N/A" : id.ToString();
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
